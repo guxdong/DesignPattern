d2c30b9b609623464e5a16acaba495cbbee8b22f
Robertt-G <xdgu@foxmail.com>
2024-03-17 15:47:20 +0800

    添加单例模式代码

---
+++ Creative/singleton.go
@@ -0,0 +1,50 @@
+package creative
+
+import "sync"
+
+type singleton struct{}
+
+var instance *singleton
+var iOnce sync.Once
+var mutex sync.Mutex
+
+// init init函数在main函数之前执行，并且是自动执行，每个包中可以有多个init函数，每个源文件中也可以有多个init函数。
+func init() {
+	if instance == nil {
+		instance = &singleton{}
+	}
+}
+
+// GetHungryInstance 饿汉模式
+func GetHungryInstance() *singleton {
+	return instance
+}
+
+// GetLazyInstance 懒汉模式
+func GetLazyInstance() *singleton {
+	mutex.Lock()
+	if instance == nil {
+		instance = &singleton{}
+	}
+	mutex.Unlock()
+	return instance
+}
+
+// GetLazyInstanceDoubleCheck 双重检查单例模式
+func GetLazyInstanceDoubleCheck() *singleton {
+	if instance == nil {
+		mutex.Lock()
+		if instance == nil {
+			instance = &singleton{}
+		}
+		mutex.Unlock()
+	}
+	return instance
+}
+
+func GetInstance() *singleton {
+	iOnce.Do(func() {
+		instance = &singleton{}
+	})
+	return instance
+}
