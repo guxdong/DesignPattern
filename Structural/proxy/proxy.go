a7a4e2071dfaa92b4126ae5e79407f6bbe12c3f3
Robertt-G <xdgu@foxmail.com>
2024-03-27 22:12:53 +0800

    添加代理模式代码

---
+++ Structural/proxy/proxy.go
@@ -0,0 +1,47 @@
+package proxy
+
+import "fmt"
+
+type HouseSeller interface {
+	SellHouse(address, buyer string)
+}
+
+// HouseSeller 房屋出售者
+type HouseOwner struct{}
+
+func (h *HouseOwner) SellHouse(address, buyer string) {
+	fmt.Printf("最终商讨价格后，与%s签署购买地址为%s的购房协议。\n", buyer, address)
+}
+
+// HouseProxy 房产中介
+type HouseProxy struct {
+	houseSeller HouseSeller
+}
+
+func NewHouseProxy(houseSeller HouseSeller) *HouseProxy {
+	return &HouseProxy{
+		houseSeller: houseSeller,
+	}
+}
+
+// viewHouse 看房介绍基本情况
+func (h *HouseProxy) viewHouse(address string, buyer string) {
+	fmt.Printf("带买家%s看位于%s的房屋，并介绍基本情况\n", buyer, address)
+}
+
+// preBargain 初步沟通价格
+func (h *HouseProxy) preBargain(address string, buyer string) {
+	fmt.Println("讨价还价后，初步达成购买意向")
+}
+
+// SellHouse 中介卖房，看房->初步谈价->最终和房东签协议
+func (h *HouseProxy) SellHouse(address string, buyer string) {
+	h.viewHouse(address, buyer)
+	h.preBargain(address, buyer)
+	h.houseSeller.SellHouse(address, buyer)
+}
+
+func Logics() {
+	proxy := NewHouseProxy(&HouseOwner{})
+	proxy.SellHouse("北京市海淀区中关村大街，2号院1号楼4单元502室", "李四")
+}
